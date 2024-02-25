41a3bdb61cd2e5de6589e292c0ee72aa5c886cb8
AndryWinston <alexkilla1@yandex.ru>
2024-02-25 20:44:15 +0300

    JRTB-1: added repository layer

--- src/main/java/com/AlexeyKalitin/javarushtelegrambot/command/HelpCommand.java
+++ src/main/java/com/AlexeyKalitin/javarushtelegrambot/command/HelpCommand.java
@@ -10,9 +10,10 @@ public class HelpCommand implements Command {
     public final static String HELP_MESSAGE = String.format("✨<b>Доcтупные команды</b>✨\n\n"
                     + "<b>Начать\\закончить работу с ботом</b>\n"
                     + "%s - начать работу со мной\n"
-                    + "%s - приостановить работу со мной\n\n"
-                    + "%s - получить помощь в работе со мной\n",
-            START.getCommandName(), STOP.getCommandName(), HELP.getCommandName());
+                    + "%s - приостановить работу со мной\n"
+                    + "%s - получить помощь в работе со мной\n"
+                    + "%s - получить мою статистику\n",
+            START.getCommandName(), STOP.getCommandName(), HELP.getCommandName(), STAT.getCommandName());
 
     public HelpCommand(SendBotMessageService sendBotMessageService) {
         this.sendBotMessageService = sendBotMessageService;
